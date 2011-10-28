2cfeaf62c33bff2e790a8a1faf3073c3072389ff
Steve Kondik <shade@chemlab.org>
2011-10-28 11:04:43 -0700

    passion: Fix PRODUCT_LOCALES
    Change-Id: I138985297a6191f5e82e48c0dbd75611e46d50cf

--- full_passion.mk
+++ full_passion.mk
@@ -25,6 +25,9 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base.mk)
 #$(call inherit-product, device/sample/products/backup_overlay.mk)
 $(call inherit-product, device/htc/passion/passion_us.mk)
 
+# Passion uses high-density artwork where available 
+PRODUCT_LOCALES += hdpi 
+
 # Discard inherited values and use our own instead.
 PRODUCT_NAME := full_passion
 PRODUCT_DEVICE := passion
